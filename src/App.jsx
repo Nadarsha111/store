5fb7f37f7031c9546dc9d78136145128b1f6d9fd
Nadarsha111 A <nadarsha565@gmail.com>
2024-04-17 06:58:50 +0000

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import TopNav from './components/TopNav'
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
 
@@ -8,7 +9,11 @@ function App() {
     <>
 
 <TopNav/>
+<div>
 
+<img src='https://static.vecteezy.com/system/resources/previews/008/174/590/large_2x/fashion-advertising-web-banner-illustration-vector.jpg' className="w-100 object-fit-fill h-25"/>
+
+</div>
     </>
   )
 }
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -2,19 +2,24 @@
 
 
 const TopNav = () => {
+
+
   return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light py-4 px-5">
 
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
 
-      <a className="navbar-brand " href="#">Navbar</a>
+      <a className="navbar-brand " href="#">
+        <img src="https://w7.pngwing.com/pngs/621/196/png-transparent-e-commerce-logo-logo-e-commerce-electronic-business-ecommerce-angle-text-service.png" width="50" height="50" className="d-inline-block align-top object-fit-fill" alt=""/>
+      
+      </a>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+  <div className="collapse navbar-collapse d-lg-flex justify-content-around" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-link" href="#">Home </a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">Link</a>
@@ -34,9 +39,9 @@ const TopNav = () => {
         <a className="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
-    <form className="form-inline my-2 my-lg-0">
+    <form className="my-2 my-lg-0 form-inline d-inline-flex d-md-none d-sm-none">
       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button className="btn btn-outline-success my-2 my-sm-0 ms-5" type="submit">Search</button>
     </form>
   </div>
 </nav>   )
