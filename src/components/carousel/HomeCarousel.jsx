e62c4e21cf8d4c1b937aefa63cad172ef1777d44
Nadarsha111 <nadarsha565@gmail.com>
2024-04-23 18:53:53 +0530

    Update scrollbar styles in App.css and add Image component to HomeCarousel.jsx

--- src/components/carousel/HomeCarousel.jsx
+++ src/components/carousel/HomeCarousel.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
+import { Carousel, Image } from "react-bootstrap";
 
 const HomeCarousel = () => {
   return (
     <Carousel className="mb-3">
       <Carousel.Item>
-        <img
+        <Image
           src="https://images.pexels.com/photos/168927/pexels-photo-168927.jpeg?auto=compress&cs=tinysrgb&w=600"
-          className="w-100 object-fit-fill h-25"
+          className="w-100 object-fit-fill  "
         />
       </Carousel.Item>
       <Carousel.Item>
         {/* <ExampleCarouselImage text="Second slide" /> */}
-        <img
+        <Image
           src="https://images.pexels.com/photos/1639729/pexels-photo-1639729.jpeg?auto=compress&cs=tinysrgb&w=600"
-          className="w-100 object-fit-fill h-25"
+          className="w-100 object-fit-fill  "
         />
       </Carousel.Item>
       <Carousel.Item>
         {/* <ExampleCarouselImage text="Third slide" /> */}
-        <img
+        <Image
           src="https://images.pexels.com/photos/7679464/pexels-photo-7679464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          className="w-100 object-fit-fill h-25"
+          className="w-100 object-fit-fill  "
         />
       </Carousel.Item>
     </Carousel>
