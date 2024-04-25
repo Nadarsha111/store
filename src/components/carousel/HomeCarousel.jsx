233022ede85927efdc6a2da9aebe2ebc5ffb8062
Nadarsha111 <nadarsha565@gmail.com>
2024-04-25 14:01:14 +0530

    Update carousel images in HomeCarousel.jsx

--- src/components/carousel/HomeCarousel.jsx
+++ src/components/carousel/HomeCarousel.jsx
@@ -3,25 +3,28 @@ import { Carousel, Image } from "react-bootstrap";
 
 const HomeCarousel = () => {
   return (
-    <Carousel className="mb-3">
-      <Carousel.Item>
+    <Carousel className="mb-3 " interval={null}>
+      <Carousel.Item className="">
         <Image
           src="https://images.pexels.com/photos/168927/pexels-photo-168927.jpeg?auto=compress&cs=tinysrgb&w=600"
-          className="w-100 object-fit-fill  "
+          className="w-100 object-fit-cover"
+          style={{ height: "75vh" }}
         />
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item className="">
         {/* <ExampleCarouselImage text="Second slide" /> */}
         <Image
           src="https://images.pexels.com/photos/1639729/pexels-photo-1639729.jpeg?auto=compress&cs=tinysrgb&w=600"
-          className="w-100 object-fit-fill  "
+          className="w-100 object-fit-cover"
+          style={{ height: "75vh" }}
         />
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item className="">
         {/* <ExampleCarouselImage text="Third slide" /> */}
         <Image
           src="https://images.pexels.com/photos/7679464/pexels-photo-7679464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          className="w-100 object-fit-fill  "
+          className="w-100 object-fit-cover"
+          style={{ height: "75vh" }}
         />
       </Carousel.Item>
     </Carousel>
